193bcc08d558db29ed1bddfe3dc8eb06efb2157c
Ayang0101 <qiufeng0101@gmail.com>
2025-09-03 18:06:54 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["111.231.101.221"];
-pub const RS_PUB_KEY: &str = "c6lvZA1jOIuBNqMAGQz8OcfSnMbsJt8OGJng9vRyG9A=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["111.231.102.255"];
+pub const RS_PUB_KEY: &str = "c6lvZA1jOIuBNqQz8OcfSnMbsJt8OGJng9vRyG9A=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
